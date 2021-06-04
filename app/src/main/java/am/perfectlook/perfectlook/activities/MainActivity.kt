3e01e3e569ee150a8c30200e26527821912850e3
Andranik Voskanyan <andranik.voskanyan@realschool.am>
2021-06-04 15:35:28 +0400

    added log in screen added new background images deleted button and edit text styles added view style

---
+++ app/src/main/java/am/perfectlook/perfectlook/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package am.perfectlook.perfectlook.activities
+
+import am.perfectlook.perfectlook.R
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/am/perfectlook/perfectlook/authentication/SignupActivity.kt
+++ app/src/main/java/am/perfectlook/perfectlook/authentication/SignupActivity.kt
@@ -1,11 +1,11 @@
-package am.perfectlook.perfectlook
+package am.perfectlook.perfectlook.authentication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
